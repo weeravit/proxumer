e1c0bb8dea9201c61a9c1b9aa110dd6e3b01c9f0
Weeravit Sirita <weeravit.sirita@gmail.com>
2020-03-19 22:02:28 +0700

    add bottom navigation with blank fragments

---
+++ app/src/main/java/com/weeravit/proxumer/AchievementFragment.kt
@@ -0,0 +1,21 @@
+package com.weeravit.proxumer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AchievementFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_achievement, container, false)
+    }
+
+    companion object {
+        fun newInstance() =
+            AchievementFragment()
+    }
+}
--- app/src/main/java/com/weeravit/proxumer/MainActivity.kt
+++ app/src/main/java/com/weeravit/proxumer/MainActivity.kt
@@ -1,12 +1,82 @@
 package com.weeravit.proxumer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
+import com.ncapdevi.fragnav.FragNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragNavController.RootFragmentListener {
+
+    private val fragNavController by lazy {
+        FragNavController(supportFragmentManager, R.id.container)
+    }
+
+    private val fragments by lazy {
+        listOf(
+            HomeFragment.newInstance(),
+            WalletFragment.newInstance(),
+            AchievementFragment.newInstance(),
+            ProfileFragment.newInstance()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragments()
+        initBottomNavigation()
     }
+
+    private fun initFragments() {
+        fragNavController.apply {
+            rootFragments = fragments
+            initialize(FragNavController.TAB1)
+        }
+    }
+
+    private fun initBottomNavigation() {
+        val homeTab =
+            AHBottomNavigationItem(R.string.home_tab, R.drawable.ic_home, R.color.inactive_tab)
+        val walletTab =
+            AHBottomNavigationItem(
+                R.string.wallet_tab,
+                R.drawable.ic_credit_card,
+                R.color.inactive_tab
+            )
+        val achievementTab =
+            AHBottomNavigationItem(
+                R.string.achievement_tab,
+                R.drawable.ic_stars,
+                R.color.inactive_tab
+            )
+        val profileTab =
+            AHBottomNavigationItem(
+                R.string.profile_tab,
+                R.drawable.ic_account_circle,
+                R.color.inactive_tab
+            )
+
+        bottom_navigation.apply {
+            addItems(listOf(homeTab, walletTab, achievementTab, profileTab))
+            accentColor = ContextCompat.getColor(this@MainActivity, R.color.active_tab)
+            inactiveColor = ContextCompat.getColor(this@MainActivity, R.color.inactive_tab)
+            isForceTint = true
+            titleState = AHBottomNavigation.TitleState.ALWAYS_HIDE
+            setNotification("1", 3)
+
+            setOnTabSelectedListener { position, _ ->
+                fragNavController.switchTab(position)
+                true
+            }
+        }
+    }
+
+    override val numberOfRootFragments: Int
+        get() = fragments.size
+
+    override fun getRootFragment(index: Int) = fragments[index]
 }
