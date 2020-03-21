e58dd33d1bc249d7665b868728e638bbc2852bc0
Weeravit Sirita <weeravit.sirita@gmail.com>
2020-03-22 02:08:02 +0700

    fix ui

--- app/src/main/java/com/weeravit/proxumer/achievement/AchievementFragment.kt
+++ app/src/main/java/com/weeravit/proxumer/achievement/AchievementFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.weeravit.proxumer.R
+import com.weeravit.proxumer.utils.EqualSpacingItemDecoration
 import kotlinx.android.synthetic.main.fragment_achievement.*
 
 class AchievementFragment : Fragment() {
+    private val achievementViewModel by viewModels<AchievementViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,53 +24,28 @@ class AchievementFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initInstances()
+        initViewModel()
     }
 
-    private fun initInstances() {
-        val achievementModel = AchievementModel(
-            level = 2,
-            achievementMenu = arrayListOf(
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                ),
-                AchievementMenuModel(
-                    title = "",
-                    imageUrl = "https://cdn.urldecoder.org/assets/images/url-fb.png"
-                )
-            )
-        )
+    private fun initViewModel() {
+        achievementViewModel.apply {
+            achievement.observe(viewLifecycleOwner, Observer {
+                text_achievement_level.text = getString(R.string.achievement_level, it.level)
 
-        text_achievement_level.text = getString(R.string.achievement_level, achievementModel.level)
+                list_achievement.apply {
+                    layoutManager = GridLayoutManager(this@AchievementFragment.context, 3)
+                    addItemDecoration(
+                        EqualSpacingItemDecoration(
+                            context = context,
+                            dimenRes = R.dimen.spacing_medium,
+                            displayMode = EqualSpacingItemDecoration.GRID
+                        )
+                    )
+                    adapter = AchievementMenuAdapter(it.achievementMenu)
+                }
+            })
 
-        list_achievement.apply {
-            layoutManager = GridLayoutManager(this@AchievementFragment.context, 3)
-            adapter = AchievementMenuAdapter(achievementModel.achievementMenu)
+            getAchievement()
         }
     }
 
